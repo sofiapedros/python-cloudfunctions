fa669fd1b5835ee6c4285f2509524cf93718f59c
SofiaPedros <112613396+sofiapedros@users.noreply.github.com>
2025-03-24 09:16:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ def http_hello_world(request: Request):
     request_json = request.get_json(silent=True)
     name = request_json.get('name', 'World') if request_json else 'World'
     
-    return f"Hello, {name} from Cloud Functions!", 200
+    return f"Hello from our group, {name} from Cloud Functions!", 200
 
