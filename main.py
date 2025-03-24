2beb5e5bbd26f6e2ab94aa746899b80e14965c02
sofiapedros <202108146@alu.comillas.edu>
2025-03-24 09:17:34 +0100

    Updated message

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from google.cloud import storage
 
+
 def process_file(event, context):
     """Triggered when a file is uploaded to Cloud Storage."""
-    file_name = event['name']
-    bucket_name = event['bucket']
-    
+    file_name = event["name"]
+    bucket_name = event["bucket"]
+
     print(f"File {file_name} uploaded to {bucket_name}. Processing...")
 
     storage_client = storage.Client()
@@ -12,17 +13,19 @@ def process_file(event, context):
     blob = bucket.blob(file_name)
 
     # Leer contenido (solo si es un archivo de texto)
-    if file_name.endswith('.txt'):
+    if file_name.endswith(".txt"):
         content = blob.download_as_text()
         print(f"File content: {content}")
-    
+
     return f"Processed {file_name} successfully!"
+
+
 from flask import Request
+
 
 def http_hello_world(request: Request):
     """HTTP Function that responds with a greeting."""
     request_json = request.get_json(silent=True)
-    name = request_json.get('name', 'World') if request_json else 'World'
-    
-    return f"Hello from our group, {name} from Cloud Functions!", 200
+    name = request_json.get("name", "World") if request_json else "World"
 
+    return f"Hello from our group, uploaded {name} from Cloud Functions!", 200
